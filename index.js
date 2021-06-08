ac036080174438d8979b5a525e6da36c52b7615e
zeejay21 <>
2021-06-08 03:03:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let cargoHold = [1138, 'space suits', 'parrot', 'instruction manual', 'meal packs', 'space tether', '20 meters'];
+
+//Use splice to make the following changes to the cargoHold array. Be sure to print the array after each step to confirm your updates.
+
+//a) Insert the string 'keys' at index 3 without replacing any other entries.
+console.log(cargoHold);
+cargoHold.splice(3,0,"keys");
+console.log(cargoHold);
+
+
+//b) Remove ‘instruction manual’ from the array. (Hint: indexOf is helpful to avoid manually counting an index).
+console.log(cargoHold.indexOf("instruction manual"));
+
+cargoHold.splice(4,1);
+
+console.log(cargoHold);
+//c) Replace the elements at indexes 2 - 4 with the items ‘cat’, ‘fob’, and ‘string cheese’.
+cargoHold.splice(2,3,"cat","fob","string cheese");
+console.log(cargoHold);
